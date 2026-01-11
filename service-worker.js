6ac43f6916b930642affcb4ee705bbbcc8fcafe2
dadouch wassim <jfgz085@gmail.com>
2026-01-11 11:53:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,54 @@
+const CACHE_NAME = 'ben10-heroic-run-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/game.js',
+    '/manifest.json',
+    '/assets/images/omnitrix.png',
+    '/assets/fonts/ben10.ttf',
+    '/assets/sounds/jump.mp3',
+    '/assets/sounds/transform.mp3',
+    '/assets/sounds/coin.mp3',
+    '/assets/sounds/game-over.mp3',
+    '/icons/icon-512.png'
+];
 
+// Installation du Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Stratégie de cache: Network First, fallback au cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                // Mettre à jour le cache avec la nouvelle réponse
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME)
+                    .then(cache => cache.put(event.request, responseClone));
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
